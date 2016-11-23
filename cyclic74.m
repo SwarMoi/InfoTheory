de1b88ad9f3c6e821a1d08eadc11c36cf0de1ba0
SwarMoi <swarnendu.moitra@gmail.com>
2016-11-24 01:04:37 +0530

    debuged,added input!
    Generator polynomial had errors, msg word input provided!

--- cyclic74.m
+++ cyclic74.m
@@ -1,14 +1,18 @@
 clc;
 clear all;
-n=7;
-k=4;
-p=[1 1 0 ; 1 1 1; 0 0 1 ; 1 0 1]; % Parity Matix
-d=[1 1 0 1]; % Message word
+n=7;k=4;
+p=[1 1 0 ; 0 1 1; 1 1 1 ; 1 0 1]; % Parity Matix
+d=input('The msg word : ');
+%d=[1 0 0 1]; % Message word
 ik=eye(k);
-g=cat(2,ik,p);disp('Generator Matrix:');disp(g);
-g1=cyclpoly(n,k,'max');
-gp=poly2sym(g1);
-disp('Generator Polynomial:');disp(gp);
+g=cat(2,p,ik);
+disp('Generator Matrix:');disp(g);
+%g1=cyclpoly(n,k) %unneccesary
+%gp=poly2sym(g1);
+disp('Message Word:');disp(d);
 c1=mtimes(d,g);
 c=mod(c1,2);
-disp('The codeword for given message is:'); disp(c);
+gp=poly2sym(c);
+disp('Generator Polynomial:');disp(gp);
+disp('The codeword is:');
+disp(c);
