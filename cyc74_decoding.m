930e6c5cf484c6868ad9c733ea57d155f2a62661
SwarMoi <swarnendu.moitra@gmail.com>
2016-11-24 02:04:26 +0530

    Cyclic Decoding
    Needs Debugging!

---
+++ cyc74_decoding.m
@@ -0,0 +1,42 @@
+%Decoding
+clc;
+clear all;
+p=[1 1 0 ; 0 1 1; 1 1 1 ; 1 0 1]; % Parity Matix
+r=[1 1 0 0 0 1 0];disp('received word of 7 bit:');disp(r);
+n=7;k=4;
+ink=eye(n-k);
+h=cat(2,ink,p');
+ht=h'
+%disp('Transpose of parity check matrics :');disp(ht);
+
+s0=r*ht;
+s=mod(s0,2);
+%disp('Syndrome :');disp(s);
+
+%
+g1=cyclpoly(n,k,'max');
+gp=poly2sym(g1);
+rp=poly2sym(r);
+[qp,remp]=quorem(rp,gp);
+%
+%disp('Syndrome polynomial:');disp(qp);
+%rem=sym2poly(remp);
+
+if (s == 0)
+disp('The received code is correct.');
+else
+disp('The received code is incorrect.');
+row = 0;
+for j=1:1:n 
+m=xor(s,ht(j,:))
+if (m==0)
+row = j
+break;
+end
+end
+r(1,row) = ~r(1,row);
+%disp(r);
+disp('Correct codeword is:');
+end
+%disp('c=');
+%disp(r);
