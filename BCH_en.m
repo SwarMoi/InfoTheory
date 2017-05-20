50287a6201bd434529f59a09c603b919c9f850bf
SwarMoi <swarnendu.moitra@gmail.com>
2017-05-20 14:36:30 +0530

    Update BCH_en.m

--- BCH_en.m
+++ BCH_en.m
@@ -1,18 +1,28 @@
-m = 4; n = 2^m-1; % Codeword length
-k = 5; % Message length
-nwords = 10; % Number of words to encode
+m =input('The msg word m : '); n = 2^m-1; % Codeword length
+k = input ('The msg length : ') ; % Message length
+nwords = input('No. of words to encode : ');% Number of words to encode 
 msg = gf(randi([0 1],nwords,k));
+disp(msg)
 % Find t, the error-correction capability.
 [genpoly,t] = bchgenpoly(n,k);
+disp('Generator Polynomial : ')
+disp(genpoly)
+disp('No of errors')
+disp(t)
 % Define t2, the number of errors to add in this example.
 t2 = t;
 
 % Encode the message.
 code = bchenc(msg,n,k);
+disp('Encoded msg : ')
+disp(code)
 % Corrupt up to t2 bits in each codeword.
 noisycode = code + randerr(nwords,n,1:t2);
+disp('Code + Noise : ')
+disp(noisycode)
 % Decode the noisy code.
 [newmsg,err,ccode] = bchdec(noisycode,n,k);
+
 if ccode==code
    disp('All errors were corrected.')
 end
